9d262304437c2932a4791fce5b6016c8ddac3935
Higor Metodio <higor.metodio@outlook.com>
2023-10-20 11:12:11 -0300

    solucao do desafio 1019 - conversao de tempo

---
+++ iniciante/20_ConversaoTempo/Program.cs
@@ -0,0 +1,7 @@
+﻿int tempo = Convert.ToInt32(Console.ReadLine());
+TimeSpan timeSpan = TimeSpan.FromSeconds(tempo);
+int hour = (int)timeSpan.TotalHours;
+int minutes = timeSpan.Minutes;
+int seconds = timeSpan.Seconds;
+
+Console.WriteLine($"{hour}:{minutes}:{seconds}");
