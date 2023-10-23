89fb1b6d7d17c8aa07ebe6ceeb155f2a43e82847
Higor Metodio <higor.metodio@outlook.com>
2023-10-23 14:43:50 -0300

    solucao dos problemas 1020 e 1021

---
+++ iniciante/21_IdadeEmDias/Program.cs
@@ -0,0 +1,10 @@
+﻿int idade = Convert.ToInt32(Console.ReadLine());
+
+int anos = Math.DivRem(idade, 365, out int result);
+int meses = Math.DivRem(result, 30, out int dias);
+
+//Math.DivRem(anos, 30, out meses)
+
+Console.WriteLine($"{anos} ano(s)");
+Console.WriteLine($"{meses} mes(es)");
+Console.WriteLine($"{dias} dia(s)");
---
+++ iniciante/22_NotasMoedas/Program.cs
@@ -0,0 +1,24 @@
+﻿
+double[] notas = new double[] { 100.0, 50.0, 20.0, 10.0, 5.0, 2.0 };
+double[] moedas = new double[] { 1, 0.50, 0.25, 0.10, 0.05, 0.01 };
+
+double valor = Convert.ToDouble(Console.ReadLine());
+int novoValor = Convert.ToInt32(valor * 100);
+
+Console.WriteLine("NOTAS:");
+foreach (double nota in notas)
+{
+    int notaConvertida = Convert.ToInt32(nota * 100);
+    int quantidade = novoValor / notaConvertida;
+    Console.WriteLine($"{quantidade} nota(s) de R$ {nota:F2}");
+    novoValor = novoValor % notaConvertida;
+}
+
+Console.WriteLine("MOEDAS:");
+foreach (double moeda in moedas)
+{
+    int moedaConvertida = Convert.ToInt32(moeda * 100);
+    int quantidade = novoValor / moedaConvertida;
+    Console.WriteLine($"{quantidade} moeda(s) de R$ {moeda:F2}");
+    novoValor = novoValor % moedaConvertida;
+}
