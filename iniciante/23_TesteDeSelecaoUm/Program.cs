04e5c3803867d944d3b8459084f0a991a004ee81
Higor Metodio <higor.metodio@outlook.com>
2023-10-25 10:31:27 -0300

    solucao desafios 1035 e 1036

---
+++ iniciante/23_TesteDeSelecaoUm/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] valores = Console.ReadLine().Split();
+int valorA = Convert.ToInt32(valores[0]);
+int valorB = Convert.ToInt32(valores[1]);
+int valorC = Convert.ToInt32(valores[2]);
+int valorD = Convert.ToInt32(valores[3]);
+
+int somaAB = valorA + valorB;
+int somaCD = valorC + valorD;
+
+if (valorB > valorC && valorD > valorA && somaCD > somaAB && valorC >= 0 && valorD >= 0 && valorA % 2 == 0)
+{
+    Console.WriteLine("Valores aceitos");
+}
+else
+{
+    Console.WriteLine("Valores nao aceitos");
+}
---
+++ iniciante/24_FormulaBhaskara/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] valores = Console.ReadLine().Split();
+
+double valorA = Convert.ToDouble(valores[0]);
+double valorB = Convert.ToDouble(valores[1]);
+double valorC = Convert.ToDouble(valores[2]);
+
+double delta = Math.Pow(valorB, 2.0) - 4 * valorA * valorC;
+
+if (valorA == 0.0 || delta < 0.0)
+{
+    Console.WriteLine("Impossivel calcular");
+}
+else
+{
+    double raiz1 = (-valorB + Math.Sqrt(delta)) / (2 * valorA);
+    double raiz2 = (-valorB - Math.Sqrt(delta)) / (2 * valorA);
+    Console.WriteLine($"R1 = {raiz1:F5}");
+    Console.WriteLine($"R2 = {raiz2:F5}");
+}
+
+
