788146657750631b74f4a1210f0c2adf64d8e600
Higor Metodio <higor.metodio@outlook.com>
2023-10-26 09:43:50 -0300

    solucao para os problemas 1037-Intervalo e 1038-Lanche do beecrowd

---
+++ iniciante/26_Lache/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] lancheQuantidade = Console.ReadLine().Split();
+string codLanche = lancheQuantidade[0];
+double qtdeLanche = Convert.ToDouble(lancheQuantidade[1]);
+
+Dictionary<string, double> tabelaLanches = new Dictionary<string, double>()
+{
+    {"1", 4.0},
+    {"2", 4.5},
+    {"3", 5.0},
+    {"4", 2.0},
+    {"5", 1.5}
+};
+
+Console.WriteLine($"Total: R$ {tabelaLanches[codLanche] * qtdeLanche:F2}");
+
