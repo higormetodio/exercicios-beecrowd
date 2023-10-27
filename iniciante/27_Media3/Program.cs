d08d03b13884a0881face05ab505900dd08e3b64
Higor Metodio <higor.metodio@outlook.com>
2023-10-27 13:02:08 -0300

    solucao do problema 1040-Media

---
+++ iniciante/27_Media3/Program.cs
@@ -0,0 +1,43 @@
+﻿string[] notas = Console.ReadLine().Split();
+double nota1 = Convert.ToDouble(notas[0]);
+double nota2 = Convert.ToDouble(notas[1]);
+double nota3 = Convert.ToDouble(notas[2]);
+double nota4 = Convert.ToDouble(notas[3]);
+
+double media = (nota1 * 2.0 + nota2 * 3.0 + nota3 * 4.0 + nota4 * 1.0) / 10.0;
+
+Console.WriteLine($"Media: {Math.Round(media, 1):F1}");
+
+if (media >= 5.0 && media <= 6.9)
+{
+    AlunoEmExame(Math.Round(media, 1));
+}
+else if (media >= 7.0)
+{
+    Console.WriteLine("Aluno aprovado.");
+}
+else
+{
+    Console.WriteLine("Aluno reprovado.");
+}
+
+static void AlunoEmExame(double media)
+{
+    Console.WriteLine("Aluno em exame.");
+
+    double notaExame = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Nota do exame: {notaExame:F1}");
+
+    double mediaFinal = (media + notaExame) / 2.0;
+
+    if (mediaFinal >= 5.0)
+    {
+        Console.WriteLine("Aluno aprovado.");
+    }
+    else
+    {
+        Console.WriteLine("Aluno reprovado.");
+    }
+
+    Console.WriteLine($"Media final: {mediaFinal:F1}");
+}
