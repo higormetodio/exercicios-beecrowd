455e44e0f5b5c2efcde3e2271da44ee361052038
Higor Metodio <higor.metodio@outlook.com>
2023-10-31 14:50:15 -0300

    solucao dos problemas 1041 e 1042

---
+++ iniciante/28_CoordenadasPonto/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] coordenadas = Console.ReadLine().Split();
+
+double coordenadaX = Convert.ToDouble(coordenadas[0]);
+double coordenadaY = Convert.ToDouble(coordenadas[1]);
+
+if (coordenadaX != 0 && coordenadaY == 0)
+{
+    Console.WriteLine("Eixo X");
+}
+else if (coordenadaX == 0 && coordenadaY != 0)
+{
+    Console.WriteLine("Eixo Y");
+}
+else if (coordenadaX > 0 && coordenadaY > 0)
+{
+    Console.WriteLine("Q1");
+}
+else if (coordenadaX < 0 && coordenadaY > 0)
+{
+    Console.WriteLine("Q2");
+}
+else if (coordenadaX < 0 && coordenadaY < 0)
+{
+    Console.WriteLine("Q3");
+}
+else if (coordenadaX > 0 && coordenadaY < 0)
+{
+    Console.WriteLine("Q4");
+}
+else
+{
+    Console.WriteLine("Origem");
+}
---
+++ iniciante/29_SortSimples/Program.cs
@@ -0,0 +1,44 @@
+﻿string[] valores = Console.ReadLine().Split();
+int[] numeros = new int[]
+{
+    Convert.ToInt32(valores[0]),
+    Convert.ToInt32(valores[1]),
+    Convert.ToInt32(valores[2]),
+};
+
+int[] copias = new int[]
+{
+    Convert.ToInt32(valores[0]),
+    Convert.ToInt32(valores[1]),
+    Convert.ToInt32(valores[2]),
+};
+
+int ehMenor;
+int contador = 0;
+
+while (contador < numeros.Length - 1)
+{
+    if (numeros[contador + 1] < numeros[contador])
+    {
+        ehMenor = numeros[contador + 1];
+        numeros[contador + 1] = numeros[contador];
+        numeros[contador] = ehMenor;
+        contador = 0;
+    }
+    else
+    {
+        contador++;
+    }
+}
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+Console.WriteLine();
+
+foreach (int copia in copias)
+{
+    Console.WriteLine(copia);
+}
