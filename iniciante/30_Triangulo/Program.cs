b2ba62bcf0b9aca1a011e01a567c80ddfa312631
Higor Metodio <higor.metodio@outlook.com>
2023-11-01 10:46:53 -0300

    solucao problema 1043-Triangulo

---
+++ iniciante/30_Triangulo/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] valores = Console.ReadLine().Split();
+double[] lados = new double[3];
+double[] copia = new double[3];
+int contador = 0;
+double menor = 0.0;
+
+for (int i = 0; i < valores.Length; i++)
+{
+    lados[i] = Convert.ToDouble(valores[i]);
+}
+
+Array.Copy(lados, copia, 3);
+
+while (contador < lados.Length - 1)
+{
+    if (lados[contador + 1] < lados[contador])
+    {
+        menor = lados[contador + 1];
+        lados[contador + 1] = lados[contador];
+        lados[contador] = menor;
+        contador = 0;
+    }
+    else
+    {
+        contador++;
+    }
+}
+
+if (lados[0] + lados[1] > lados[2])
+{
+    double soma = lados[0] + lados[1] + lados[2];
+    Console.WriteLine($"Perimetro = {soma:F1}");
+}
+else
+{
+    double area = (copia[0] + copia[1]) / 2 * copia[2];
+    Console.WriteLine($"Area = {area:F1}");
+}
+
+
