741d12bb64c7e04487683174ac9254393c896444
Higor Metodio <higor.metodio@outlook.com>
2023-11-03 10:40:54 -0300

    solucao dos problemas 1044 - Multiplos e 1045 - Tipos de Triângulos

---
+++ iniciante/31_Multiplos/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] valores = Console.ReadLine().Split();
+int valorA = Convert.ToInt32(valores[0]);
+int valorB = Convert.ToInt32(valores[1]);
+
+if (valorA % valorB == 0 || valorB % valorA == 0)
+{
+    Console.WriteLine("Sao Multiplos");
+}
+else
+{
+    Console.WriteLine("Nao sao Multiplos");
+}
---
+++ iniciante/32_TiposDeTriangulo/Program.cs
@@ -0,0 +1,54 @@
+﻿string[] valores = Console.ReadLine().Split();
+int contador = 0;
+
+double[] valoresOrdenados = new double[]
+{
+    Convert.ToDouble(valores[0]),
+    Convert.ToDouble(valores[1]),
+    Convert.ToDouble(valores[2])
+};
+
+while (contador < valoresOrdenados.Length - 1)
+{
+    if (valoresOrdenados[contador] < valoresOrdenados[contador + 1])
+    {
+        double maior = valoresOrdenados[contador + 1];
+        valoresOrdenados[contador + 1] = valoresOrdenados[contador];
+        valoresOrdenados[contador] = maior;
+        contador = 0;
+    }
+    else
+    {
+        contador++;
+    }
+}
+
+double ladoA = valoresOrdenados[0];
+double ladoB = valoresOrdenados[1];
+double ladoC = valoresOrdenados[2];
+
+if (ladoA >= ladoB + ladoC)
+{
+    Console.WriteLine("NAO FORMA TRIANGULO");
+}
+else if (Math.Pow(ladoA, 2) == Math.Pow(ladoB, 2) + Math.Pow(ladoC, 2))
+{
+    Console.WriteLine("TRIANGULO RETANGULO");
+}
+else if (Math.Pow(ladoA, 2) > Math.Pow(ladoB, 2) + Math.Pow(ladoC, 2))
+{
+    Console.WriteLine("TRIANGULO OBTUSANGULO");
+}
+else
+{
+    Console.WriteLine("TRIANGULO ACUTANGULO");
+}
+
+if (ladoA == ladoB && ladoA == ladoC)
+{
+    Console.WriteLine("TRIANGULO EQUILATERO");
+}
+else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+{
+    Console.WriteLine("TRIANGULO ISOSCELES");
+}
