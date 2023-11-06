e4e91bc463d44a87193e3684023e989d8a0dbc5c
Higor Metodio <higor.metodio@outlook.com>
2023-11-06 13:53:16 -0300

    solucao dos problemas 1046 - Tempo de Jogo e 1047 - Tempo de Jogo com Minutos

---
+++ iniciante/33_TempoDeJogo/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] tempos = Console.ReadLine().Split();
+
+int horaInicio = Convert.ToInt32(tempos[0]);
+int horaFim = Convert.ToInt32(tempos[1]);
+
+TimeSpan tempoTotal = TimeSpan.FromHours(horaFim + 24 - horaInicio);
+
+if (tempoTotal.Hours == 0)
+{
+    Console.WriteLine("O JOGO DUROU 24 HORA(S)");
+}
+else
+{
+    Console.WriteLine($"O JOGO DUROU {tempoTotal.Hours} HORA(S)");
+}
+
---
+++ iniciante/34_TempoDeJogoEmMinutos/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] tempos = Console.ReadLine().Split();
+int horaInicial = Convert.ToInt32(tempos[0]);
+int minutoInicial = Convert.ToInt32(tempos[1]);
+int horaFinal = Convert.ToInt32(tempos[2]);
+int minutoFinal = Convert.ToInt32(tempos[3]);
+
+int tempoInicial = horaInicial * 60 + minutoInicial;
+int tempoFinal = horaFinal * 60 + minutoFinal;
+
+int tempoTotal = 0;
+
+if (tempoFinal > tempoInicial)
+{
+    tempoTotal = tempoFinal - tempoInicial;
+}
+else
+{
+    tempoTotal = tempoFinal + 1440 - tempoInicial;
+}
+
+if (tempoTotal == 0)
+{
+    Console.WriteLine("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+}
+else
+{
+    Console.WriteLine($"O JOGO DUROU {tempoTotal / 60} HORA(S) E {tempoTotal % 60} MINUTO(S)");
+}
+
+
+
+
