af7ae68756a7fa6b645819267288dd6460e6585d
Higor Metodio <higor.metodio@outlook.com>
2023-11-09 15:37:31 -0300

    solucao dos problemas 1050-DDD e 1051-Imposto de Renda

---
+++ iniciante/37_Ddd/Program.cs
@@ -0,0 +1,20 @@
+﻿Dictionary<int, string> ddd = new Dictionary<int, string>()
+{
+    {11, "Sao Paulo"},
+    {71, "Salvador"},
+    {61, "Brasilia"},
+    {21, "Rio de Janeiro"},
+    {32, "Juiz de Fora"},
+    {19, "Campinas"},
+    {27, "Vitoria"},
+    {31, "Belo Horizonte"}
+};
+
+int valor = Convert.ToInt32(Console.ReadLine());
+
+if (ddd.ContainsKey(valor))
+    Console.WriteLine(ddd[valor]);
+else
+    Console.WriteLine("DDD nao cadastrado");
+
+
---
+++ iniciante/38_ImpostoDeRenda/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+if (salario >= 0.0m && salario <= 2000.00m)
+{
+    Console.WriteLine("Isento");
+}
+else if (salario >= 2000.01m && salario <= 3000.00m)
+{
+    decimal faixa = salario - 2000.00m;
+    decimal imposto = faixa * 0.08m;
+    Console.WriteLine($"{imposto:C}", CultureInfo.InvariantCulture);
+}
+else if (salario >= 3000.01m && salario <= 4500.00m)
+{
+    decimal faixa = salario - 3000.00m;
+    decimal faixa2 = salario - faixa - 2000.00m;
+    decimal imposto = faixa * 0.18m + faixa2 * 0.08m;
+    Console.WriteLine($"{imposto:C}", CultureInfo.InvariantCulture);
+}
+else
+{
+    decimal faixa = salario - 4500.00m;
+    decimal faixa2 = salario - faixa - 3000.00m;
+    decimal faixa3 = salario - faixa2 - faixa - 2000.00m;
+    decimal imposto = faixa * 0.28m + faixa2 * 0.18m + faixa3 * 0.08m;
+    Console.WriteLine($"{imposto:C}", CultureInfo.InvariantCulture);
+}
