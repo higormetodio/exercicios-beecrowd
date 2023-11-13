5f76b64d702a3d2cafcf2fca07354b8b90f4e383
Higor Metodio <higor.metodio@outlook.com>
2023-11-13 14:13:35 -0300

    solucao para o problema 1052-Mes

---
+++ iniciante/39_Mes/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+
+int mes = Convert.ToInt32(Console.ReadLine());
+
+DateTimeFormatInfo month = new DateTimeFormatInfo();
+Console.WriteLine(month.GetMonthName(mes));
+
+
