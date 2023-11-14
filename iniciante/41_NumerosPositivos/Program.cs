3d64caa524b4427807d84f83223c6425e16f7a69
Higor Metodio <higor.metodio@outlook.com>
2023-11-14 10:36:26 -0300

    solucao problema 1060 - Numeros Positivos

---
+++ iniciante/41_NumerosPositivos/Program.cs
@@ -0,0 +1,20 @@
+﻿int contador = 0;
+double[] numeros = new double[6];
+while (contador < 6)
+{
+    numeros[contador] = Convert.ToDouble(Console.ReadLine());
+    contador++;
+}
+
+//Poderíamos utilizar o método count com um expressão lambda
+//int teste = numeros.Count(x => x > 0);
+//Console.WriteLine(teste);
+contador = 0;
+
+foreach (double numero in numeros)
+{
+    if (numero > 0)
+        contador++;
+}
+
+Console.WriteLine($"{contador} valores positivos");
