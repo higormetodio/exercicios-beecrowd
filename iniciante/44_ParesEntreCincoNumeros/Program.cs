b938cb4719488de7cc866d7e00f9ca56150bcee1
Higor Metodio <higor.metodio@outlook.com>
2023-11-17 10:24:10 -0300

    solucao dos problemas 1065-Pares entre cinco numeros e 1066-Pares, impares, positivos e negativos

---
+++ iniciante/44_ParesEntreCincoNumeros/Program.cs
@@ -0,0 +1,29 @@
+﻿int contador = 1;
+int pares = 0;
+
+while (contador <= 5)
+{
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        pares++;
+    }
+    contador++;
+}
+
+Console.WriteLine($"{pares} valores pares");
+// int[] valores = new int[5];
+// int contador = 0;
+
+// for (int i = 0; i < 5; i++)
+// {
+//     valores[i] = Convert.ToInt32(Console.ReadLine());
+
+//     if (valores[i] % 2 == 0)
+//     {
+//         contador++;
+//     }
+// }
+
+// Console.WriteLine($"{contador} valores pares");
---
+++ iniciante/45_ParesImparesPositivosNegativos/Program.cs
@@ -0,0 +1,38 @@
+﻿int contador = 1;
+int pares = 0, impares = 0, positivos = 0, negativos = 0;
+
+while (contador <= 5)
+{
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero > 0 && numero % 2 == 0)
+    {
+        positivos++;
+        pares++;
+    }
+    else if (numero > 0 && numero % 2 != 0)
+    {
+        positivos++;
+        impares++;
+    }
+    else if (numero < 0 && numero % 2 == 0)
+    {
+        negativos++;
+        pares++;
+    }
+    else if (numero < 0 && numero % 2 != 0)
+    {
+        negativos++;
+        impares++;
+    }
+    else
+    {
+        pares++;
+    }
+    contador++;
+}
+
+Console.WriteLine($"{pares} valor(es) par(es)");
+Console.WriteLine($"{impares} valor(es) impar(es)");
+Console.WriteLine($"{positivos} valor(es) positivo(s)");
+Console.WriteLine($"{negativos} valor(es) negativo(s)");
