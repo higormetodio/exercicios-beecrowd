876b90b52b058724335370812e40d1a15a467968
Higor Metodio <higor.metodio@outlook.com>
2023-11-18 10:09:50 -0300

    Atualizando e organizando o repositório dos exercicios beecrow

---
+++ iniciante/46_NumerosImpares/Program.cs
@@ -0,0 +1,12 @@
+﻿int valor = Convert.ToInt32(Console.ReadLine());
+
+if (valor >= 1 && valor <= 1000)
+{
+    for (int i = 1; i <= valor; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ iniciante/47_SeisNumerosImpares/Program.cs
@@ -0,0 +1,13 @@
+﻿int valor = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= 6)
+{
+    if (valor % 2 != 0)
+    {
+        Console.WriteLine(valor);
+        valor++;
+        count++;
+    }
+    valor++;
+}
