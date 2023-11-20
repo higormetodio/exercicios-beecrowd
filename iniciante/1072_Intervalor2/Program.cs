b884e7a126e447e94deb3619c28cbe6bef151900
Higor Metodio <higor.metodio@outlook.com>
2023-11-20 12:27:34 -0300

    solucao para o problema 1072-Intervalo 2

---
+++ iniciante/1072_Intervalor2/Program.cs
@@ -0,0 +1,42 @@
+﻿int numero = Convert.ToInt32(Console.ReadLine());
+int contadorIn = 0;
+int contadorOut = 0;
+
+if (numero < 10000)
+{
+    int[] arrayInteiros = new int[numero];
+
+    for (int i = 0; i < numero; i++)
+    {
+        int valor = Convert.ToInt32(Console.ReadLine());
+
+        if (valor > Math.Pow(-10, 7) && valor < Math.Pow(10, 7))
+        {
+            arrayInteiros[i] = valor;
+        }
+    }
+
+    //loop para verificar os números que estão dentro do intervaor [10, 20]
+    for (int i = 0; i < arrayInteiros.Length; i++)
+    {
+        if (arrayInteiros[i] >= 10 && arrayInteiros[i] <= 20)
+        {
+            contadorIn++;
+        }
+        else
+        {
+            contadorOut++;
+        }
+    }
+    //Poderíamos utilizar expressão lambda para exercutar a mesma operação
+    // contadorIn = arrayInteiros.Count(x => x >= 10 && x <= 20);
+    // contadorOut = arrayInteiros.Length - contadorIn;
+
+    Console.WriteLine($"{contadorIn} in");
+    Console.WriteLine($"{contadorOut} out");
+}
+
+
+
+
+
---
+++ iniciante/1072_Intervalor2/README.md
@@ -0,0 +1,16 @@
+# PROBLEMA 1072 - INTERVALO 2 - UTILIZANDO C#
+
+Leia um valor inteiro **N**. Este valor será a quantidade de valores inteiros **X** que serão lidos em seguida.
+Mostre quantos destes valores **X** estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando essas informações.
+
+## Entrada
+A primeira linha da entrada contém um valor inteiro **N** (**N** < 10000), que indica o número de casos de teste.
+Cada caso de teste a seguir é um valor inteiro **X** (-10<sup>7</sup> < **X** < 10<sup>7</sup>).
+
+## Saída
+Para cada caso, imprima quantos números estão dentro (**in**) e quantos valores estão fora (**out**) do intervalo.
+
+
+| Exemplo de Entrada          | Exemplor de Saída    |
+|-----------------------------|----------------------|
+| 4<br>14<br>123<br>10<br>-25 |**2 in**<br>**2 out** |  
