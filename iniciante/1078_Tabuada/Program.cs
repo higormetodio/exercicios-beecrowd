d59ec1ce66725c5c7407e356f79e5deed48116e5
Higor Metodio <higor.metodio@outlook.com>
2023-11-22 09:48:03 -0300

    solucao problema iniciante 1078-Tabuada

---
+++ iniciante/1078_Tabuada/Program.cs
@@ -0,0 +1,9 @@
+﻿int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 2 && numero < 1000)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($"{i} x {numero} = {i * numero}");
+    }
+}
