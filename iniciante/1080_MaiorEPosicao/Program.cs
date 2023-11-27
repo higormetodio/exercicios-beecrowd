abff5934f813ba0c7ce6bfbc487c2fdc82bc9b86
Higor Metodio <higor.metodio@outlook.com>
2023-11-27 10:15:08 -0300

    solucao do problema 1080-Maior e Posicao

---
+++ iniciante/1080_MaiorEPosicao/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] valores = new int[100];
+int numero;
+
+for (int i = 0; i < 100; i++)
+{
+    numero = Convert.ToInt32(Console.ReadLine());
+    valores[i] = numero;
+}
+
+int maior = valores[0];
+int posicao = 0;
+
+// foreach (int valor in valores)
+// {
+//     if (valor > maior)
+//     {
+//         maior = valor;
+//         posicao = Array.IndexOf(valores, valor);
+//     }
+// }
+
+for (int j = 0; j < valores.Length; j++)
+{
+    if (valores[j] > maior)
+    {
+        maior = valores[j];
+        posicao = j + 1;
+    }
+}
+
+Console.WriteLine(maior);
+Console.WriteLine(posicao);
---
+++ iniciante/1080_MaiorEPosicao/README.md
@@ -0,0 +1,14 @@
+# PROBLEMA 1080 - MAIOR E POSIÇÃO - UTILIZANDO C#
+
+Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+
+## Entrada
+O arquivo de entrada contém 100 números inteiros, positivos e distintos.
+
+## Saída
+Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
+
+
+| Exemplo de Entrada                     | Exemplor de Saída |
+|----------------------------------------|-------------------|
+| 2<br>13<br>45<br>34565<br>6<br>...<br>8| **34565<br>4**    |
