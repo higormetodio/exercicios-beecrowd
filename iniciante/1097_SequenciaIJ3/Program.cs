9e7fc8b780be71ccb086f297638a6dd7d6d13efa
Higor Metodio <higor.metodio@outlook.com>
2023-11-30 09:45:39 -0300

    solucao prblema 1097-Sequencia IJ 3

--- iniciante/1097_SequenciaIJ3/Program.cs
+++ iniciante/1097_SequenciaIJ3/Program.cs
@@ -1,13 +1,32 @@
 ﻿int auxiliar = 7;
 
+//Primeira Solução
+// for (int i = 1; i <= 9; i += 2)
+// {
+//     int j = auxiliar;
+//     while (j >= auxiliar - 2)
+//     {
+//         Console.WriteLine($"I={i} J={j}");
+//         j--;
+//     }
+
+//     auxiliar = j + 5;
+// }
+
+//Segunda solução
 for (int i = 1; i <= 9; i += 2)
 {
-    int j = auxiliar;
-    while (j >= auxiliar - 2)
+    int auxiliar2 = 0;
+
+    for (int j = auxiliar; j >= auxiliar - 2; j--)
     {
         Console.WriteLine($"I={i} J={j}");
-        j--;
+
+        if (j == auxiliar - 2)
+        {
+            auxiliar2 = j;
+        }
     }
 
-    auxiliar = j + 5;
+    auxiliar = auxiliar2 + 4;
 }
