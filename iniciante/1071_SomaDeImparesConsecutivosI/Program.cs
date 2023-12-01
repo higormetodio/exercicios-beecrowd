e045e2f38ba3cd99c7e66927ac36cfcbf02e6d70
Higor Metodio <higor.metodio@outlook.com>
2023-12-01 10:38:47 -0300

    melhoria no código e retirada das variáveis para controle do maior e menor número

--- iniciante/1071_SomaDeImparesConsecutivosI/Program.cs
+++ iniciante/1071_SomaDeImparesConsecutivosI/Program.cs
@@ -1,23 +1,19 @@
 ﻿int primeiroValor = Convert.ToInt32(Console.ReadLine());
 int segundoValor = Convert.ToInt32(Console.ReadLine());
-int menorValor;
-int maiorValor;
+int maior;
+
 int soma = 0;
 
 //Verificando o menor e o maior número;
-if (primeiroValor < segundoValor)
+if (primeiroValor > segundoValor)
 {
-    menorValor = primeiroValor;
-    maiorValor = segundoValor;
-}
-else
-{
-    menorValor = segundoValor;
-    maiorValor = primeiroValor;
+    maior = primeiroValor;
+    primeiroValor = segundoValor;
+    segundoValor = maior;
 }
 
 //Loop para verificar os valores entre o menor (exclusive) e o maior (exclusive)
-for (int i = menorValor + 1; i < maiorValor; i++)
+for (int i = primeiroValor + 1; i < segundoValor; i++)
 {
     //soma dos valores ímpares
     if (i % 2 != 0)
