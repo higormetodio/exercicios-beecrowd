a3bec20cb1f999459334f927afab24c8a2ce6869
Higor Metodio <higor.metodio@outlook.com>
2023-12-01 11:33:57 -0300

    solucao do problema 1101-Sequencia de numeros e soma

---
+++ iniciante/1101_SequenciaDeNumerosESoma/Program.cs
@@ -0,0 +1,36 @@
+﻿// Digitar dois números inteiros
+// Verificar se o primeiro número é maior que o segundo
+// Inverter os números caso a condição anterior seja satisfeita
+// Repetir a sequeência abaixo até que um dos números digitados seja > 0 (zero)
+// Passar entre o primeiro número digitado até o segundo número digitado
+// imprimir esses números na mesma linha
+// aplicar a soma de cada um dos números
+// imprimir a soma quato todos números foram somados
+// Digitar dois números inteiros
+
+string[] valores = Console.ReadLine().Split(' ');
+int primeiroValor = Convert.ToInt32(valores[0]);
+int segundoValor = Convert.ToInt32(valores[1]);
+
+while (primeiroValor > 0 && segundoValor > 0)
+{
+    int soma = 0;
+    if (primeiroValor > segundoValor)
+    {
+        int maior = primeiroValor;
+        primeiroValor = segundoValor;
+        segundoValor = maior;
+    }
+
+
+    for (int i = primeiroValor; i <= segundoValor; i++)
+    {
+        Console.Write($"{i} ");
+        soma += i;
+    }
+    Console.WriteLine($"Sum={soma}");
+
+    valores = Console.ReadLine().Split(' ');
+    primeiroValor = Convert.ToInt32(valores[0]);
+    segundoValor = Convert.ToInt32(valores[1]);
+}
---
+++ iniciante/1101_SequenciaDeNumerosESoma/README.md
@@ -0,0 +1,16 @@
+# PROBLEMA 1101 - SEQUÊNCIA DE NÚMEROS E SOMA - UTILIZANDO C#
+
+Leia um conjunto não determinado de pares de valores **M e N** (parar quando algum dos valores for menor ou igual a zero). Para cada par lido, mostre a sequência do menor até o maior e a soma dos inteiros consecutivos entre eles (incluindo o **N e M**).
+
+## Entrada
+O arquivo de entrada contém um número não determinado de valores **M e N**. A última linha de entrada vai conter um número nulo ou negativo.
+
+## Saída
+Para cada dupla de valores, imprima a sequência do menor até o maior e a soma deles, conforme exemplo abaixo.
+
+
+| Exemplo de Entrada | Exemplor de Saída |
+|--------------------|-------------------|
+| 5 2                | **2 3 4 5 Sum=14**|  
+| 6 3                | **3 4 5 6 Sum=18**|
+| 5 0                |                   |
