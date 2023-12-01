a8648e7d6ad694a712c125e4511449b01c7a8b27
Higor Metodio <higor.metodio@outlook.com>
2023-12-01 10:37:31 -0300

    solucao problema 1099-Soma de Impares Consecutivos II

---
+++ iniciante/1099_SomaDeImparesConsecutivos2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Ler o número N de casos a serem testados
+// inicializar um totalizador com zero
+// percorrer até que o número de testes seja satisfeito
+// Ler dois números na mesma linha
+// Verificar se o primeiro número é ou não maior do que o segundo número
+// inverter os números em caso da condição anterior ser verdadeira
+// percorrer os números exclusive
+// verificar se o número percorrido na condição anterior é impar
+// somar o número se a condição anterior for atendida
+
+using System.Globalization;
+
+int numero = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < numero; i++)
+{
+    string[] valores = Console.ReadLine().Split(' ');
+    int primeiroValor = Convert.ToInt32(valores[0]);
+    int segundoValor = Convert.ToInt32(valores[1]);
+    int soma = 0;
+
+    if (primeiroValor > segundoValor)
+    {
+        int maior = primeiroValor;
+        primeiroValor = segundoValor;
+        segundoValor = maior;
+    }
+
+    for (int j = primeiroValor + 1; j < segundoValor; j++)
+    {
+        if (j % 2 != 0)
+        {
+            soma += j;
+        }
+    }
+    Console.WriteLine(soma);
+}
+
+
---
+++ iniciante/1099_SomaDeImparesConsecutivos2/README.md
@@ -0,0 +1,16 @@
+# PROBLEMA 1099 - SOMA ÍMPARES CONSECUTIVOS II - UTILIZANDO C#
+
+Leia um valor inteiro **N** que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste de dois inteiros **X e Y**. Você deve apresentar a soma de todos os ímpares existentes **entre X e Y**.
+
+## Entrada
+A primeira linha de entrada é um inteiro **N** que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste em uma linha contendo dois inteiros **X e Y**.
+
+## Saída
+Imprima a soma de todos valores ímpares **entre X e Y**
+
+
+| Exemplo de Entrada | Exemplor de Saída |
+|--------------------|-------------------|
+| 6<br>-5            | **5**             |  
+| 12<br>15           | **13**            |
+| 12<br>12           | **0**             |
