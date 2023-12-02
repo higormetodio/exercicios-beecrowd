970ac757bbdd65999d40a79e52c67570b32ea890
Higor Metodio <higor.metodio@outlook.com>
2023-12-02 09:57:04 -0300

    solucao problema 1114-Senha Fixa

---
+++ iniciante/1114_SenhaFixa/Program.cs
@@ -0,0 +1,17 @@
+﻿int senha = 2002;
+int valor;
+
+do
+{
+    valor = Convert.ToInt32(Console.ReadLine());
+
+    if (senha == valor)
+    {
+        Console.WriteLine("Acesso Permitido");
+    }
+    else
+    {
+        Console.WriteLine("Senha Invalida");
+    }
+
+} while (senha != valor);
