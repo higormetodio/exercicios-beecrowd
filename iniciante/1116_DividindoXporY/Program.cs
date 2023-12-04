a232601bed493297a14cbe98fc7a4ef5039a20f7
Higor Metodio <higor.metodio@outlook.com>
2023-12-04 09:48:18 -0300

    solucao problema 1116-Divisao X por Y

---
+++ iniciante/1116_DividindoXporY/Program.cs
@@ -0,0 +1,17 @@
+﻿int numero = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= numero; i++)
+{
+    string[] valores = Console.ReadLine().Split(' ');
+    int valorX = Convert.ToInt32(valores[0]);
+    int valorY = Convert.ToInt32(valores[1]);
+
+    if (valorY == 0)
+    {
+        Console.WriteLine("divisao impossivel");
+    }
+    else
+    {
+        Console.WriteLine($"{valorX / (double)valorY:F1}");
+    }
+}
