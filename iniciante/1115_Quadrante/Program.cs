dacde876b2a330e7e7c386f52570ae660ea2e301
Higor Metodio <higor.metodio@outlook.com>
2023-12-04 09:23:23 -0300

    solucao do problema 1115-Quadrante

---
+++ iniciante/1115_Quadrante/Program.cs
@@ -0,0 +1,36 @@
+﻿// Ler duas coordenadas
+// Verificar se a variável X ou Y é zero.
+// Caso de sim, ecerra o programa
+// Do contrário verifica as condilções abaixo:
+// Se X maior que zero e Y maior que zero imprimir: primeiro
+// Se X maior que zero e Y menor que zero imprimir: quarto
+// Se X menor que zero e Y menor que zero imprimir: terceiro
+// Se X menor que zero e Y maior que zero imprimir: segundo
+
+string[] valores = Console.ReadLine().Split(' ');
+int valorX = Convert.ToInt32(valores[0]);
+int valorY = Convert.ToInt32(valores[1]);
+
+while (valorX != 0 && valorY != 0)
+{
+    if (valorX > 0 && valorY > 0)
+    {
+        Console.WriteLine("primeiro");
+    }
+    else if (valorX > 0 && valorY < 0)
+    {
+        Console.WriteLine("quarto");
+    }
+    else if (valorX < 0 && valorY < 0)
+    {
+        Console.WriteLine("terceiro");
+    }
+    else
+    {
+        Console.WriteLine("segundo");
+    }
+
+    valores = Console.ReadLine().Split(' ');
+    valorX = Convert.ToInt32(valores[0]);
+    valorY = Convert.ToInt32(valores[1]);
+}
---
+++ iniciante/1115_Quadrante/README.md
@@ -0,0 +1,18 @@
+# PROBLEMA 1115 - QUADRANTE - UTILIZANDO C#
+
+Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma). 
+
+## Entrada
+A entrada contém vários casos de teste. Cada caso de teste contém 2 valores inteiros.
+
+## Saída
+Para cada caso de teste mostre em qual quadrante do sistema cartesiano se encontra a coordenada lida, conforme o exemplo.
+
+
+| Exemplo de Entrada | Exemplor de Saída   |
+|--------------------|-------------------  |
+| 2 2                | **primeiroa**       |  
+| 3 -2               | **quarto**          |
+| -8 -1              | **terceiro**        |
+| -7 1               | **segundo**         |
+| 0 2                |                     |
