2a1c2dc8792e395065d799fa32b26c21e2c8c234
Higor Metodio <higor.metodio@outlook.com>
2023-12-05 13:54:33 -0300

    solucao problema 1117-Validação de Notas

---
+++ iniciante/1117_ValidacaoDeNotas/Program.cs
@@ -0,0 +1,21 @@
+﻿int contador = 0;
+double soma = 0;
+
+do
+{
+    double nota = Convert.ToDouble(Console.ReadLine());
+
+    if (nota >= 0.0 && nota <= 10.0)
+    {
+        contador++;
+        soma += nota;
+
+    }
+    else
+    {
+        Console.WriteLine("nota invalida");
+    }
+
+} while (contador < 2);
+
+Console.WriteLine($"media = {soma / 2:F2}");
