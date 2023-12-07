cc3b626b3abedb5e7440a23d76a2249071504fd7
Higor Metodio <higor.metodio@outlook.com>
2023-12-07 10:48:50 -0300

    solucao do problema 1134_Tipo de Combustível

---
+++ iniciante/1134_TipoDeCombustivel/Program.cs
@@ -0,0 +1,28 @@
+﻿bool validacao = true;
+(int alcool, int gasolina, int diesel) = (0, 0, 0);
+
+while (validacao)
+{
+    int resposta = Convert.ToInt32(Console.ReadLine());
+
+    switch (resposta)
+    {
+        case 1:
+            alcool++;
+            break;
+        case 2:
+            gasolina++;
+            break;
+        case 3:
+            diesel++;
+            break;
+        case 4:
+            validacao = false;
+            break;
+    }
+}
+
+Console.WriteLine("MUITO OBRIGADO");
+Console.WriteLine($"Alcool: {alcool}");
+Console.WriteLine($"Gasolina: {gasolina}");
+Console.WriteLine($"Diesel: {diesel}");
