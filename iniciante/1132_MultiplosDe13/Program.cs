be4bd32f7add6ef6fc98d0b51d5195a79ab12d30
Higor Metodio <higor.metodio@outlook.com>
2023-12-07 10:14:49 -0300

    solucao problema 1132-Multiplos de 13

---
+++ iniciante/1132_MultiplosDe13/Program.cs
@@ -0,0 +1,20 @@
+﻿int numeroX = Convert.ToInt32(Console.ReadLine());
+int numeroY = Convert.ToInt32(Console.ReadLine());
+int somaNaoMultiplos = 0;
+
+if (numeroX > numeroY)
+{
+    int maior = numeroX;
+    numeroX = numeroY;
+    numeroY = maior;
+}
+
+for (int i = numeroX; i <= numeroY; i++)
+{
+    if (i % 13 != 0)
+    {
+        somaNaoMultiplos += i;
+    }
+}
+
+Console.WriteLine(somaNaoMultiplos);
