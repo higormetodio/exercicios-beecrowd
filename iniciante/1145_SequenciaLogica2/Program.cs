be2d8ec3684f6c10f705eddc77b1bb6161dd09cc
Higor Metodio <higor.metodio@outlook.com>
2023-12-09 15:02:24 -0300

    solucao problema 1145-Sequencia Logica 2

---
+++ iniciante/1145_SequenciaLogica2/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] valores = Console.ReadLine()!.Split(' ');
+int valorX = Convert.ToInt32(valores[0]);
+int valorY = Convert.ToInt32(valores[1]);
+int contador = 1;
+
+if (valorX > 1 && valorX < 20 && valorY > valorX && valorY < 100000)
+{
+    for (int i = 1; i <= valorY; i++)
+    {
+        if (contador < valorX)
+        {
+            Console.Write($"{i} ");
+            contador++;
+        }
+        else
+        {
+            Console.WriteLine($"{i}");
+            contador = 1;
+        }
+    }
+}
+
