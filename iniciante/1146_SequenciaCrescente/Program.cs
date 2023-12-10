8d7f095e2d8b84522b69541e650c34a5496d884b
Higor Metodio <higor.metodio@outlook.com>
2023-12-10 14:54:58 -0300

    solucao problema 1146-Sequencia Crescente

---
+++ iniciante/1146_SequenciaCrescente/Program.cs
@@ -0,0 +1,13 @@
+﻿int valor = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    for (int i = 1; i < valor; i++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine(valor);
+
+    valor = Convert.ToInt32(Console.ReadLine());
+
+} while (valor != 0);
