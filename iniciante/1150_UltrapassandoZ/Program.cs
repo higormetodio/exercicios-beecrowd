58dcd2525a0d5bc7deb3356bbe94d25425300b16
Higor Metodio <higor.metodio@outlook.com>
2023-12-12 10:10:08 -0300

    solucao problema 1150-Ultrapassando Z

---
+++ iniciante/1150_UltrapassandoZ/Program.cs
@@ -0,0 +1,18 @@
+﻿int valorX = Convert.ToInt32(Console.ReadLine());
+int valorZ;
+int soma = 0;
+int contador = 0;
+
+do
+{
+    valorZ = Convert.ToInt32(Console.ReadLine());
+} while (valorZ <= valorX);
+
+while (soma < valorZ)
+{
+    soma += valorX;
+    valorX++;
+    contador++;
+}
+
+Console.WriteLine(contador);
