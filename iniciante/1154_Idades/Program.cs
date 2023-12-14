c451dc738abae2c28f84f02e0fde1358e087ac95
Higor Metodio <higor.metodio@outlook.com>
2023-12-14 10:17:31 -0300

    solucao problema 1154-Idades

---
+++ iniciante/1154_Idades/Program.cs
@@ -0,0 +1,14 @@
+﻿int contador = 0;
+int soma = 0;
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+while (idade > 0)
+{
+    contador++;
+    soma += idade;
+
+    idade = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"{(double)soma / contador:F2}");
