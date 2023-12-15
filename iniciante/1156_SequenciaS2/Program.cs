40c88853461c09744fae5f5463c35e49d5d9185b
Higor Metodio <higor.metodio@outlook.com>
2023-12-15 18:54:54 -0300

    solucao problema 1156-Sequencia S II

---
+++ iniciante/1156_SequenciaS2/Program.cs
@@ -0,0 +1,12 @@
+﻿double valorS = 1;
+int impar = 1;
+int dobro = 2;
+
+while (impar <= 39)
+{
+    impar += 2;
+    valorS += (double)impar / dobro;
+    dobro *= 2;
+}
+
+Console.WriteLine($"{valorS:F2}");
