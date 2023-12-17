418293dd4b33726b516a7bc323378e935c74d219
Higor Metodio <higor.metodio@outlook.com>
2023-12-17 17:46:35 -0300

    solucao problema 1158-Soma de Impares Consecutivos III

---
+++ iniciante/1158_SomaImparesConsecutivos3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices.Marshalling;
+
+int testes = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= testes; i++)
+{
+    string[] valores = Console.ReadLine().Split();
+    int valorX = Convert.ToInt32(valores[0]);
+    int valorY = Convert.ToInt32(valores[1]);
+    int soma = 0;
+    int nImpares = 0;
+
+    do
+    {
+        if (valorX % 2 != 0)
+        {
+            soma += valorX;
+            valorX++;
+            nImpares++;
+        }
+        else
+        {
+            valorX++;
+        }
+    } while (nImpares < valorY);
+
+    Console.WriteLine(soma);
+}
