21ef8407e0c81ebb46268753e7d54603d8783fa2
Higor Metodio <higor.metodio@outlook.com>
2023-12-22 12:11:01 -0300

    solucao do problema 1165-Numero Primo

---
+++ iniciante/1165_NumeroPrimo/Program.cs
@@ -0,0 +1,23 @@
+﻿int casosTestes = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= casosTestes; i++)
+{
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    bool ehPrimo = true;
+
+    for (int j = 2; j <= Math.Sqrt(numero); j++)
+    {
+        if (numero <= 1 || numero % j == 0)
+        {
+            Console.WriteLine($"{numero} nao eh primo");
+            ehPrimo = false;
+            break;
+        }
+    }
+
+    if (ehPrimo)
+    {
+        Console.WriteLine($"{numero} eh primo");
+    }
+}
