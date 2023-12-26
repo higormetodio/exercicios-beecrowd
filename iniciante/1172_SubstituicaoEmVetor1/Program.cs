38002cf7ea5d1da77f38a842c10407becf8e3a98
Higor Metodio <higor.metodio@outlook.com>
2023-12-26 11:29:07 -0300

    solucao problema 1172-Substituicao em Vetor I

---
+++ iniciante/1172_SubstituicaoEmVetor1/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] vetorX = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero <= 0)
+    {
+        vetorX[i] = 1;
+    }
+    else
+    {
+        vetorX[i] = numero;
+    }
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"X[{i}] = {vetorX[i]}");
+}
