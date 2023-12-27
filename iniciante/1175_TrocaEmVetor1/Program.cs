81a5b2d1bbda0f52d156f4b9bb0373030685ab75
Higor Metodio <higor.metodio@outlook.com>
2023-12-27 10:52:29 -0300

    solucao problema 1175-Troca em Vetor I

---
+++ iniciante/1175_TrocaEmVetor1/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] vetorN = new int[20];
+
+for (int i = vetorN.Length - 1; i >= 0; i--)
+{
+    vetorN[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < vetorN.Length; i++)
+{
+    Console.WriteLine($"N[{i}] = {vetorN[i]}");
+}
