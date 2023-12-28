cc4e07b3f4f7c0490473298c7690f67f06714c19
Higor Metodio <higor.metodio@outlook.com>
2023-12-28 11:12:08 -0300

    solucao problema 1076-Fibonaccie em Vetor

---
+++ iniciante/1176_FibonacciEmVetor/Program.cs
@@ -0,0 +1,23 @@
+﻿int testes = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= testes; i++)
+{
+    int valorN = Convert.ToInt32(Console.ReadLine());
+
+    long[] fibonacci = new long[valorN + 1];
+
+    for (int j = 0; j < fibonacci.Length; j++)
+    {
+        if (j == 0)
+            fibonacci[j] = 0;
+        else if (j == 1)
+            fibonacci[j] = 1;
+        else
+        {
+            fibonacci[j] = fibonacci[j - 1] + fibonacci[j - 2];
+        }
+
+    }
+
+    Console.WriteLine($"Fib({valorN}) = {fibonacci[valorN]}");
+}
