522e3fd280abb3ab78b81ac8d32bdb012db4cec9
Higor Metodio <higor.metodio@outlook.com>
2023-12-29 15:57:01 -0300

    solucao problema 1177-Preenchimento de Vetor II

---
+++ iniciante/1177_PreenchimentoDeVetor2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] vetor = new int[1000];
+int valor = Convert.ToInt32(Console.ReadLine());
+int contador = 0;
+
+
+while (contador < vetor.Length)
+{
+    for (int i = 0; i < valor; i++)
+    {
+        if (contador < vetor.Length)
+        {
+            vetor[contador] = i;
+            contador++;
+        }
+    }
+}
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine($"N[{i}] = {vetor[i]}");
+}
+
+
