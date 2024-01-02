0661ed44ac55d4ece4a7c5b4c17829f64b8030ae
Higor Metodio <higor.metodio@outlook.com>
2024-01-02 16:55:17 -0300

    solucao problema 1178-Preenchimento de Vetor III

---
+++ iniciante/1178_PreenchimentoDeVetor3/Program.cs
@@ -0,0 +1,13 @@
+﻿decimal[] vetorN = new decimal[100];
+decimal valorX = Convert.ToDecimal(Console.ReadLine());
+vetorN[0] = valorX;
+
+for (int i = 1; i < vetorN.Length; i++)
+{
+    vetorN[i] = vetorN[i - 1] / 2;
+}
+
+for (int i = 0; i < vetorN.Length; i++)
+{
+    Console.WriteLine($"N[{i}] = {decimal.Round(vetorN[i], 4, MidpointRounding.ToEven)}");
+}
