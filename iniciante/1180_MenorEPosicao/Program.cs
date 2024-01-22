15b39e6d8fc664ff1f817d98f5d2f3011c0a9b8f
Higor Metodio <higor.metodio@outlook.com>
2024-01-22 11:24:35 -0300

    Solucao problema 1180-Menor e Posicao

---
+++ iniciante/1180_MenorEPosicao/Program.cs
@@ -0,0 +1,31 @@
+﻿int valorN = Convert.ToInt32(Console.ReadLine());
+int[] vetorX = new int[valorN];
+string[] valores = Console.ReadLine().Split(' ', valorN);
+
+for (int i = 0; i < valorN; i++)
+{
+    vetorX[i] = Convert.ToInt32(valores[i]);
+}
+
+//Utilizando funções do System.Linq
+//Console.WriteLine($"Menor valor: {vetorX.Min()}");
+//Console.WriteLine($"Posicao: {Array.IndexOf(vetorX, vetorX.Min())}");
+int menor = vetorX[0];
+int atual;
+int indice = 0;
+
+for (int i = 1; i < valorN; i++)
+{
+    atual = vetorX[i];
+
+    if (atual < menor)
+    {
+        menor = atual;
+        indice = i;
+    }
+}
+
+Console.WriteLine($"Menor valor: {menor}");
+Console.WriteLine($"Posicao: {indice}");
+
+
