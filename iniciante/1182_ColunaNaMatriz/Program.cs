6a8a6535979e4debffb0ea6dc8ab1c391440a299
Higor Metodio <higor.metodio@outlook.com>
2024-01-24 09:59:28 -0300

    Solucao problema 1182-Coluna na Matriz

---
+++ iniciante/1182_ColunaNaMatriz/Program.cs
@@ -0,0 +1,27 @@
+﻿int coluna = Convert.ToInt32(Console.ReadLine());
+char letra = Convert.ToChar(Console.ReadLine().ToUpper());
+double[,] matriz = new double[12, 12];
+double soma = 0.0;
+
+for (int i = 0; i < 12; i++)
+{
+    for (int j = 0; j < 12; j++)
+    {
+        matriz[i, j] = Convert.ToDouble(Console.ReadLine());
+
+        if (j == coluna)
+        {
+            soma += matriz[i, j];
+        }
+    }
+}
+
+if (letra == 'S')
+{
+    Console.WriteLine($"{soma:F1}");
+}
+else if (letra == 'M')
+{
+    double media = soma / 12;
+    Console.WriteLine($"{media:F1}");
+}
