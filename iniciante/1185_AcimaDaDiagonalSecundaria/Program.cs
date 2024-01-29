2e3a625cd12c612858eb293a3f97c070fd4ba387
Higor Metodio <higor.metodio@outlook.com>
2024-01-29 13:12:32 -0300

    Solucao problema 1185-Acima da Diagonal Secundaria

---
+++ iniciante/1185_AcimaDaDiagonalSecundaria/Program.cs
@@ -0,0 +1,31 @@
+﻿char letra = Convert.ToChar(Console.ReadLine().ToUpper());
+double[,] matriz = new double[12, 12];
+double soma = 0.0;
+int limite = matriz.GetLength(0) - 1;
+int contador = 0;
+
+for (int i = 0; i < matriz.GetLength(0); i++)
+{
+    for (int j = 0; j < matriz.GetLength(0); j++)
+    {
+        matriz[i, j] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+for (int i = 0; i < limite; i++)
+{
+    for (int j = 0; j < limite - i; j++)
+    {
+        soma += matriz[i, j];
+        contador++;
+    }
+}
+
+if (letra == 'S')
+{
+    Console.WriteLine($"{soma:F1}");
+}
+else if (letra == 'M')
+{
+    Console.WriteLine($"{soma / contador:F1}");
+}
