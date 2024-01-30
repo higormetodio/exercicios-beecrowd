80912a82b2311c6291cf14341f22700e5921cbd4
Higor Metodio <higor.metodio@outlook.com>
2024-01-30 14:52:55 -0300

    Solucao problema 1186-Abaixo da Diagonal Secundaria

---
+++ iniciante/1186_AbaixoDaDiagonalSecundaria/Program.cs
@@ -0,0 +1,13 @@
+﻿char letra = Convert.ToChar(Console.ReadLine().ToUpper());
+double[,] matriz = new double[12, 12];
+int limite = matriz.GetLength(0);
+
+for (int i = 0; i < limite; i++)
+{
+    for (int j = 0; j < 12; j++)
+    {
+        matriz[i, j] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+for (int i = ; i < )
