ab2d71eeca65e9667a1a7f605d80aca563750092
Higor Metodio <higor.metodio@outlook.com>
2024-01-31 10:29:15 -0300

    Solucao problema 1187-Area Superior

---
+++ iniciante/1187_AreaSuperior/Program.cs
@@ -0,0 +1,31 @@
+﻿char letra = Convert.ToChar(Console.ReadLine().ToUpper());
+double[,] matriz = new double[12, 12];
+int limite = matriz.GetLength(0);
+int controle = limite - 1;
+int contador = 0;
+double soma = 0;
+
+for (int i = 0; i < limite; i++)
+{
+    for (int j = 0; j < limite; j++)
+    {
+        matriz[i, j] = Convert.ToDouble(Console.ReadLine());
+
+        if (i + 1 <= j && j < controle)
+        {
+            soma += matriz[i, j];
+            contador++;
+        }
+    }
+
+    controle--;
+}
+
+if (letra == 'S')
+{
+    Console.WriteLine($"{soma:F1}");
+}
+else if (letra == 'M')
+{
+    Console.WriteLine($"{soma / contador:F1}");
+}
