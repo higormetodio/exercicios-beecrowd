753d0f43581ca3cb9cfb1052625597518e21b403
Higor Metodio <higor.metodio@outlook.com>
2024-02-06 10:38:44 -0300

    solucao problema 1478-Matriz Quadrada II

---
+++ iniciante/1478_MatrizQuadrada2/Program.cs
@@ -0,0 +1,31 @@
+﻿int valor = Convert.ToInt32(Console.ReadLine());
+int[,] matriz = new int[valor, valor];
+
+while (valor != 0)
+{
+    for (int i = 0; i < valor; i++)
+    {
+        for (int j = 0; j < valor; j++)
+        {
+            matriz[i, j] = Math.Abs(i - j) + 1;
+        }
+    }
+
+    for (int i = 0; i < valor; i++)
+    {
+        for (int j = 0; j < valor; j++)
+        {
+            Console.Write(matriz[i, j].ToString().PadLeft(3));
+
+            if (j < valor - 1)
+                Console.Write(" ");
+            else
+                Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+    valor = Convert.ToInt32(Console.ReadLine());
+    matriz = new int[valor, valor];
+}
+
+
