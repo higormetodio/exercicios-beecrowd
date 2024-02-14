d8b77b4b65c2ce29f162e35100208f4719290372
Higor Metodio <higor.metodio@outlook.com>
2024-02-14 18:05:23 -0300

    solucao problema 1541 - Construindo Casas

---
+++ iniciante/1541_ConstruindoCasas/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] valores = Console.ReadLine().Split(' ');
+
+while (!valores.Contains("0"))
+{
+    double lado1 = Convert.ToInt32(valores[0]);
+    double lado2 = Convert.ToInt32(valores[1]);
+    double percentual = Convert.ToInt32(valores[2]);
+
+    double valor = Math.Floor(Math.Sqrt(lado1 * lado2 / (percentual / 100)));
+
+    Console.WriteLine((int)valor);
+
+    valores = Console.ReadLine().Split();
+}
