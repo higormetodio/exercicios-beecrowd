bb3fd25ac018263f5cc42edd39d0185aeea7ca4a
Higor Metodio <higor.metodio@outlook.com>
2024-02-15 13:33:16 -0300

    Solucao problema 1557-Matriz Quadrada III

---
+++ iniciante/1557_MatrizQuadrada3/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+while (true)
+{
+    int valor = Convert.ToInt32(Console.ReadLine());
+
+    if (valor == 0)
+        break;
+
+    int[,] matriz = new int[valor, valor];
+
+    for (int i = 0; i < valor; i++)
+    {
+        double expoente = Convert.ToDouble(i);
+
+        for (int j = 0; j < valor; j++)
+        {
+            if (j == 0)
+                matriz[i, j] = Convert.ToInt32(Math.Pow(2, expoente));
+            else
+                matriz[i, j] = matriz[i, j - 1] * 2;
+        }
+    }
+
+    string digitos = matriz[valor - 1, valor - 1].ToString();
+    int t = digitos.Count();
+
+    for (int i = 0; i < valor; i++)
+    {
+        for (int j = 0; j < valor; j++)
+        {
+            if (j == 0)
+                Console.Write($"{matriz[i, j].ToString().PadLeft(t)}");
+            else
+                Console.Write($" {matriz[i, j].ToString().PadLeft(t)}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
