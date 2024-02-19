2a38e60547d4a0779ea26df5a19b61368e607861
Higor Metodio <higor.metodio@outlook.com>
2024-02-19 10:06:21 -0300

    solucao problema 1589-Bob Conduite

---
+++ iniciante/1589_BobConduite/Program.cs
@@ -0,0 +1,12 @@
+﻿int numeroCasos = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < numeroCasos; i++)
+{
+    string[] valores = Console.ReadLine().Split(' ');
+    int r1 = Convert.ToInt32(valores[0]);
+    int r2 = Convert.ToInt32(valores[1]);
+
+    int raioTotal = r1 + r2;
+
+    Console.WriteLine(raioTotal);
+}
