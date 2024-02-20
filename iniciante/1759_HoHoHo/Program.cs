8292abc9c97a00ec9e2306f9340b8d5ffd3a8e9e
Higor Metodio <higor.metodio@outlook.com>
2024-02-20 11:06:11 -0300

    solucao problema 1759 - Ho Ho Ho

---
+++ iniciante/1759_HoHoHo/Program.cs
@@ -0,0 +1,13 @@
+﻿int quantidade = Convert.ToInt32(Console.ReadLine());
+
+//Outars Opções
+//Console.Write(string.Concat(Enumerable.Repeat("Ho ", quantidade - 1)));
+//Console.WriteLine("Ho!");
+
+while (quantidade - 1 > 0)
+{
+    Console.Write("Ho ");
+    quantidade--;
+}
+
+Console.WriteLine("Ho!");
