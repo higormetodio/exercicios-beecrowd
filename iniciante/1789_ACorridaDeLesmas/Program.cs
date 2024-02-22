8ffb0decb6038279f0c8212a62c53838310ddf58
Higor Metodio <higor.metodio@outlook.com>
2024-02-22 13:27:25 -0300

    solucao do problema 1789-Corrida De Lesmas

---
+++ iniciante/1789_ACorridaDeLesmas/Program.cs
@@ -0,0 +1,28 @@
+﻿// BEECROWD 1789 - Corrida de Lesmas
+// https://judge.beecrowd.com/pt/problems/view/1789
+
+while (true)
+{
+    if (!int.TryParse(Console.ReadLine(), out int casos))
+        break;
+
+    string[] valores = Console.ReadLine().Split(' ');
+    int[] velocidades = new int[valores.Length];
+    int ehMaior = Convert.ToInt32(valores[0]);
+
+    for (int i = 0; i < valores.Length; i++)
+    {
+        if (!int.TryParse(valores[i], out velocidades[i]))
+            break;
+
+        if (i > 0 && velocidades[i] > ehMaior)
+            ehMaior = velocidades[i];
+    }
+
+    if (ehMaior < 10)
+        Console.WriteLine(1);
+    else if (ehMaior >= 10 && ehMaior < 20)
+        Console.WriteLine(2);
+    else
+        Console.WriteLine(3);
+}
