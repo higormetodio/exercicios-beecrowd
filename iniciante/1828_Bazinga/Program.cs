0919bb5677136238f601c5d24f5480cbfdcfbc07
Higor Metodio <higor.metodio@outlook.com>
2024-02-23 10:48:53 -0300

    solucao problema 1828-Bazinga!

---
+++ iniciante/1828_Bazinga/Program.cs
@@ -0,0 +1,44 @@
+﻿int casos = Convert.ToInt32(Console.ReadLine());
+string sheldon, raj;
+
+for (int i = 1; i <= casos; i++)
+{
+    string[] respostas = Console.ReadLine().ToLower().Split(' ');
+
+    sheldon = respostas[0];
+    raj = respostas[1];
+
+    if (sheldon == raj)
+        Console.WriteLine($"Caso #{i}: De novo!");
+    else if (VerificarResultado(sheldon, raj))
+        Console.WriteLine($"Caso #{i}: Bazinga!");
+    else
+        Console.WriteLine($"Caso #{i}: Raj trapaceou!");
+}
+
+static bool VerificarResultado(string sheldon, string raj)
+{
+
+    if (sheldon == "tesoura" && raj == "papel")
+        return true;
+    if (sheldon == "papel" && raj == "pedra")
+        return true;
+    if (sheldon == "pedra" && raj == "lagarto")
+        return true;
+    if (sheldon == "lagarto" && raj == "spock")
+        return true;
+    if (sheldon == "spock" && raj == "tesoura")
+        return true;
+    if (sheldon == "tesoura" && raj == "lagarto")
+        return true;
+    if (sheldon == "lagarto" && raj == "papel")
+        return true;
+    if (sheldon == "papel" && raj == "spock")
+        return true;
+    if (sheldon == "spock" && raj == "pedra")
+        return true;
+    if (sheldon == "pedra" && raj == "tesoura")
+        return true;
+
+    return false;
+}
