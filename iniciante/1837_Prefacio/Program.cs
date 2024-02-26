eb42917b93286d939239df743d3ec96a3f227866
Higor Metodio <higor.metodio@outlook.com>
2024-02-26 16:14:35 -0300

    solucao do problema 1837_Prefacio

---
+++ iniciante/1837_Prefacio/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] valores = Console.ReadLine().Split(' ');
+int valorA = Convert.ToInt32(valores[0]);
+int valorB = Convert.ToInt32(valores[1]);
+int resto;
+int quociente;
+
+for (resto = 0; resto < Math.Abs(valorB); resto++)
+{
+    if ((valorA - resto) % valorB == 0)
+    {
+        quociente = (valorA - resto) / valorB;
+        Console.WriteLine($"{quociente} {resto}");
+        break;
+    }
+}
+
