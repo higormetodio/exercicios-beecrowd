5fc05a0d1bb76fea522f0cded9114dbc6896f2b8
Higor Metodio <higor.metodio@outlook.com>
2024-02-27 20:39:44 -0300

    solucao poblema Bem Vindos e Bem Vindas ao Inverno

---
+++ iniciante/1847_BemVindosEBemVindasAoInverno/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] temperaturas = Console.ReadLine().Split(' ');
+int tempA = Convert.ToInt32(temperaturas[0]);
+int tempB = Convert.ToInt32(temperaturas[1]);
+int tempC = Convert.ToInt32(temperaturas[2]);
+
+if (tempA > tempB && tempB <= tempC)
+    Console.WriteLine(":)");
+else if (tempA < tempB && tempB >= tempC)
+    Console.WriteLine(":(");
+else if (tempA < tempB && tempB < tempC && tempC - tempB < tempB - tempA)
+    Console.WriteLine(":(");
+else if (tempA < tempB && tempB < tempC && tempC - tempB >= tempB - tempA)
+    Console.WriteLine(":)");
+else if (tempA > tempB && tempB > tempC && tempC - tempB > tempB - tempA)
+    Console.WriteLine(":)");
+else if (tempA > tempB && tempB > tempC && tempC - tempB <= tempB - tempA)
+    Console.WriteLine(":(");
+else if (tempA == tempB && tempB < tempC)
+    Console.WriteLine(":)");
+else if (tempA == tempB && tempB > tempC)
+    Console.WriteLine(":(");
+else
+    Console.WriteLine(":(");
