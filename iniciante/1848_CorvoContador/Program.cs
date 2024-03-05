74d23f5999220eaffcba26c3fdc1c13bdcd7dfff
Higor Metodio <higor.metodio@outlook.com>
2024-03-04 21:41:49 -0300

    solucao problema 1848-Corvo Contador

---
+++ iniciante/1848_CorvoContador/Program.cs
@@ -0,0 +1,22 @@
+﻿double soma = 0;
+int contador = 1;
+
+while (contador <= 3)
+{
+    string valores = Console.ReadLine();
+
+    if (valores == "caw caw")
+    {
+        Console.WriteLine(Convert.ToInt32(soma));
+        soma = 0;
+        contador++;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (valores[i] == '*')
+            soma += Math.Pow(2, 2 - i);
+    }
+}
+
+
