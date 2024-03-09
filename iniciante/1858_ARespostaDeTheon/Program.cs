5684700cbd3cdd03a31968ad5c1251463550ecfd
Higor Metodio <higor.metodio@outlook.com>
2024-03-09 15:14:11 -0300

    Solucao problema 1858-A Resposta de Theon

---
+++ iniciante/1858_ARespostaDeTheon/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+string[] t = Console.ReadLine().Split(' ');
+int[] numeros = new int[n];
+int ehMenor;
+int posicao;
+
+for (int i = 0; i < n; i++)
+{
+    numeros[i] = Convert.ToInt32(t[i]);
+}
+
+ehMenor = numeros[0];
+posicao = 1;
+
+for (int i = 1; i < n; i++)
+{
+    if (ehMenor > numeros[i])
+    {
+        ehMenor = numeros[i];
+        posicao = i + 1;
+    }
+
+}
+
+Console.WriteLine(posicao);
+
