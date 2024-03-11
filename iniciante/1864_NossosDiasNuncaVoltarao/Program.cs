2ed9e1197be9d1f13a71a7603cf39ba218f8a087
Higor Metodio <higor.metodio@outlook.com>
2024-03-11 12:46:37 -0300

    solucao do problema 1864-Nossos dias nunca voltarao

---
+++ iniciante/1864_NossosDiasNuncaVoltarao/Program.cs
@@ -0,0 +1,8 @@
+﻿string texto = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= n - 1; i++)
+{
+    Console.Write(texto[i]);
+}
+Console.WriteLine();
