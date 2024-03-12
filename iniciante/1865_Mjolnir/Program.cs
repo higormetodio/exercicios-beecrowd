3a7a7f9ed17027f97ec2de5a4715ca55b2ce6075
Higor Metodio <higor.metodio@outlook.com>
2024-03-12 12:24:57 -0300

    solucao do problema 1865-Mjolnir

---
+++ iniciante/1865_Mjolnir/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string c = Console.ReadLine();
+    c = c.Substring(0, c.IndexOf(' '));
+
+    Console.WriteLine(c == "Thor" ? "Y" : "N");
+}
