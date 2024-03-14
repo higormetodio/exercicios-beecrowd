79f9cd2154f8fc229ac9a1f329dc00a1046a597f
Higor Metodio <higor.metodio@outlook.com>
2024-03-14 13:34:22 -0300

    solucao do problema 1866-Conta

---
+++ iniciante/1866_Conta/Program.cs
@@ -0,0 +1,38 @@
+﻿int C = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < C; i++)
+{
+    int N = Convert.ToInt32(Console.ReadLine());
+    if (N % 2 == 0)
+    {
+        Console.WriteLine(0);
+    }
+    else
+    {
+        Console.WriteLine(1);
+    }
+}
+
+//Outra solução Possível
+// int C = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i < C; i++)
+// {
+//     int N = Convert.ToInt32(Console.ReadLine());
+//     int S = 1;
+
+//     for (int j = 2; j <= N; j++)
+//     {
+//         if (j % 2 == 0)
+//         {
+//             S -= 1;
+//         }
+//         else
+//         {
+//             S += 1;
+//         }
+//     }
+//     Console.WriteLine(S);
+// }
+
+
