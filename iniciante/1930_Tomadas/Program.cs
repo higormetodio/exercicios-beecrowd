12b55cffc7c2a008a4ba0a6e7ffdc0d07747e83e
Higor Metodio <higor.metodio@outlook.com>
2024-05-08 10:52:43 -0300

    solucao do problema 1930-Tomadas

---
+++ iniciante/1930_Tomadas/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] valores = Console.ReadLine().Split(' ');
+int[] numeros = new int[4];
+int soma = 0;
+
+
+for (int i = 0; i < 4; i++)
+{
+    numeros[i] = Convert.ToInt32(valores[i]);
+    soma += numeros[i];
+}
+
+Console.WriteLine(soma - 3);
+
+
