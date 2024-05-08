02a2bb8740c87cc8495f9437a75d8b815d50efd5
Higor Metodio <higor.metodio@outlook.com>
2024-05-08 11:17:19 -0300

    solucao problema 1933-Tri-Du

---
+++ iniciante/1933_TriDu/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] valores = Console.ReadLine().Split(' ');
+int[] cartas = { Convert.ToInt32(valores[0]), Convert.ToInt32(valores[1]) };
+int valorMaximo = 0;
+
+if (cartas[0] == cartas[1])
+{
+    valorMaximo = cartas[0];
+}
+else if (cartas[0] > cartas[1])
+{
+    valorMaximo = cartas[0];
+}
+else
+{
+    valorMaximo = cartas[1];
+}
+
+Console.WriteLine(valorMaximo);
