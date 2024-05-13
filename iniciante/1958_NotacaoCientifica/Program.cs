a60675530ea0d4b7831defe540db91829609d496
Higor Metodio <higor.metodio@outlook.com>
2024-05-13 15:25:16 -0300

    solucao do problema 1958-Notacao Cientifica

---
+++ iniciante/1958_NotacaoCientifica/Program.cs
@@ -0,0 +1,9 @@
+﻿string numero = Console.ReadLine();
+
+if (numero.Equals("-0"))
+    Console.WriteLine(Convert.ToDouble(numero).ToString("-0.0000E+00"));
+else if (numero.StartsWith("-") && Convert.ToDouble(numero) <= 0)
+    Console.WriteLine(Convert.ToDouble(numero).ToString("0.0000E+00"));
+else
+    Console.WriteLine(Convert.ToDouble(numero).ToString("+0.0000E+00"));
+
