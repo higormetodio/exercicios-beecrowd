4eed28188453c281e084c7e28ef57b140ed65d3f
Higor Metodio <higor.metodio@outlook.com>
2024-07-19 20:43:10 -0300

    Solucao de problemas

---
+++ iniciante/1959_PoligonosRegularesSimples/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] value = Console.ReadLine().Split(' ');
+
+int N = Convert.ToInt32(value[0]);
+long L = Convert.ToInt64(value[1]);
+
+Console.WriteLine(N * L);
---
+++ iniciante/1960_NumeracaoRomanaNumerosPagina/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] numeros = new int[] { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+string[] romanos = new string[] { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+string algarismo = "";
+int i = 0;
+int contador = 13;
+
+int valor = Convert.ToInt32(Console.ReadLine());
+
+while (i < contador)
+{
+    if (numeros[i] > valor)
+    {
+        valor -= numeros[i - 1];
+
+        for (int indice = 0; indice < contador; indice++)
+        {
+            if (valor < numeros[indice])
+            {
+                contador = indice + 1;
+                break;
+            }
+        }
+
+        algarismo += romanos[i - 1];
+        i = -1;
+    }
+    else if (numeros[i] == valor)
+    {
+        valor -= numeros[i];
+
+        for (int indice = 0; indice < contador; indice++)
+        {
+            if (valor < numeros[indice])
+            {
+                contador = indice;
+                break;
+            }
+        }
+
+        algarismo += romanos[i];
+        i = -1;
+    }
+
+    i++;
+}
+
+
+Console.WriteLine(algarismo.Trim());
