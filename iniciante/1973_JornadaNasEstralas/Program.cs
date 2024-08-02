ef9a6de3b6636b0a22457df13fcaf681e3a52d60
Higor Metodio <higor.metodio@outlook.com>
2024-08-02 15:15:47 -0300

    solucao problema 1973-Jornada nas estrelas

---
+++ iniciante/1973_JornadaNasEstralas/Program.cs
@@ -0,0 +1,42 @@
+﻿long N = long.Parse(Console.ReadLine());
+string[] valores = Console.ReadLine().Split();
+long[] X = new long[N];
+bool[] visitadas = new bool[N];
+long totalCarneiros = 0;
+int contador = 0;
+int estrelasAtacadas = 0;
+
+for (int i = 0; i < N; i++)
+{
+    X[i] = Convert.ToInt32(valores[i]);
+    totalCarneiros += X[i];
+}
+
+while (contador >= 0 && contador < N)
+{
+    long valor = X[contador];
+
+    if (valor > 0)
+    {
+        totalCarneiros--;
+        X[contador]--;
+    }
+
+    if (!visitadas[contador])
+    {
+        visitadas[contador] = true;
+        estrelasAtacadas++;
+    }
+
+
+    if (valor % 2 != 0)
+    {
+        contador++;
+    }
+    else
+    {
+        contador--;
+    }
+}
+
+Console.WriteLine($"{estrelasAtacadas} {totalCarneiros}");
