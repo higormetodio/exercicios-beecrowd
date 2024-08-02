9758891830a379fb364f24723ab9d9404c7b4201
Higor Metodio <higor.metodio@outlook.com>
2024-08-02 10:01:24 -0300

    solucao problema 1963-O Filme

---
+++ iniciante/1963_OFilme/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] valores = Console.ReadLine().Split();
+double A = Convert.ToDouble(valores[0]);
+double B = Convert.ToDouble(valores[1]);
+
+//double percentual = (B - A) / A
+double percentual = (B - A) / A * 100;
+
+//Console.WriteLine($"{percentual:P2}");
+Console.WriteLine($"{percentual:F2}%");
