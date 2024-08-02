cd5bde996f9e975eb597c666c79398486b887b49
Higor Metodio <higor.metodio@outlook.com>
2024-08-02 09:29:01 -0300

    Solucao do problema 1962-Ha muito muito tempo atras

---
+++ iniciante/1962_HaMuitoTempoAtras/Program.cs
@@ -0,0 +1,16 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+int ano = 2015;
+int T;
+for (int i = 0; i < N; i++)
+{
+    T = Convert.ToInt32(Console.ReadLine());
+
+    if (T >= ano)
+    {
+        Console.WriteLine($"{T - ano + 1} A.C.");
+    }
+    else
+    {
+        Console.WriteLine($"{ano - T} D.C.");
+    }
+}
