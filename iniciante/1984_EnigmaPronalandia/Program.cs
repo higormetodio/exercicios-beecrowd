da99689991ce5505cdd4019bd3c8962e395a4423
Higor Metodio <higor.metodio@outlook.com>
2024-08-14 11:22:20 -0300

    solucao do problema 1984-Enigma Pronalandia

---
+++ iniciante/1984_EnigmaPronalandia/Program.cs
@@ -0,0 +1,10 @@
+﻿long valor = long.Parse(Console.ReadLine());
+string valorString = valor.ToString();
+string novoValor = "";
+
+for (int i = valorString.Length - 1; i >= 0; i--)
+{
+    novoValor += valorString[i];
+}
+
+Console.WriteLine(novoValor);
