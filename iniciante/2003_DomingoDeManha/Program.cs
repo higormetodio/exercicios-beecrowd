433e26fa216d060996a4a15588924815d8d11ac6
Higor Metodio <higor.metodio@outlook.com>
2024-08-14 14:54:43 -0300

    solucao do problema 2003-Domingo de Manha

---
+++ iniciante/2003_DomingoDeManha/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    if (!TimeSpan.TryParseExact(Console.ReadLine(), "h\\:mm", null, out TimeSpan saida))
+    {
+        break;
+    }
+
+    TimeSpan percurso = TimeSpan.ParseExact("1:00", "h\\:mm", null);
+    TimeSpan horaDoEncontro = TimeSpan.ParseExact("8:00", "h\\:mm", null);
+
+    TimeSpan tempoDeChegada = saida + percurso;
+    TimeSpan atrasoMaximo = tempoDeChegada - horaDoEncontro;
+
+    if (atrasoMaximo.TotalMinutes >= 0)
+    {
+        Console.WriteLine($"Atraso maximo: {atrasoMaximo.TotalMinutes}");
+    }
+    else
+    {
+        Console.WriteLine("Atraso maximo: 0");
+    }
+}
