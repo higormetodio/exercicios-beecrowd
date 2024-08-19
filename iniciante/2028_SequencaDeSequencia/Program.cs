5c703a1e6d69831cf5f290282fe59f98f142aff3
Higor Metodio <higor.metodio@outlook.com>
2024-08-19 15:01:55 -0300

    solucao do problema 2028-Sequencia de Sequencia

---
+++ iniciante/2028_SequencaDeSequencia/Program.cs
@@ -0,0 +1,46 @@
+﻿int caso = 1;
+
+while (true)
+{
+    if (!int.TryParse(Console.ReadLine(), out int entrada))
+    {
+        break;
+    }
+
+    List<int> valores = new List<int>();
+
+    for (int i = 0; i <= entrada; i++)
+    {
+        int contInterno;
+
+        if (i == 0)
+        {
+            valores.Add(i);
+        }
+        else
+        {
+            contInterno = 1;
+
+            while (contInterno <= i)
+            {
+                valores.Add(i);
+                contInterno++;
+            }
+        }
+    }
+
+
+    if (valores.Count == 1)
+    {
+        Console.WriteLine($"Caso {caso++}: {valores.Count} numero");
+        Console.WriteLine(valores[0]);
+    }
+    else
+    {
+        Console.WriteLine($"Caso {caso++}: {valores.Count} numeros");
+        Console.WriteLine(string.Join(" ", valores));
+    }
+
+    Console.WriteLine("");
+
+}
