801bbaa41c9844a472bae9b04d8e4835dbabbc3c
Higor Metodio <higor.metodio@outlook.com>
2024-08-19 09:32:17 -0300

    solucao problema 2006-Identificando Chá

---
+++ iniciante/2006_IdentificandoCha/Program.cs
@@ -0,0 +1,13 @@
+﻿int T = Convert.ToInt32(Console.ReadLine());
+string[] respostas = Console.ReadLine().Split();
+int contador = 0;
+
+foreach (string resposta in respostas)
+{
+    if (Convert.ToInt32(resposta) == T)
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine(contador);
