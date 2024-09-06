db9746fe5f2019a1d821c73a1edb2ad8a940c52e
Higor Metodio <higor.metodio@outlook.com>
2024-09-06 11:11:18 -0300

    solucao problema 2029 - Reservatorio de Mel

---
+++ iniciante/2029_ReservatorioMel/Program.cs
@@ -0,0 +1,17 @@
+﻿
+while (true)
+{
+    if (!decimal.TryParse(Console.ReadLine(), out decimal volume))
+        break;
+
+    if (!decimal.TryParse(Console.ReadLine(), out decimal diamentro))
+        break;
+
+    decimal pi = 3.14m;
+    decimal raio = diamentro / 2;
+    decimal altura = volume / (pi * raio * raio);
+    decimal area = pi * raio * raio;
+
+    Console.WriteLine($"ALTURA = {altura:F2}");
+    Console.WriteLine($"AREA = {area:F2}");
+}
