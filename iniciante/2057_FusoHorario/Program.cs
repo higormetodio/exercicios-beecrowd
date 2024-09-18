b2da62e07dd3f4ff7ba99a41c3d21ca2ebeaf3c2
Higor Metodio <higor.metodio@outlook.com>
2024-09-18 10:40:11 -0300

    solucao problema 2057- Fuso Horario

---
+++ iniciante/2057_FusoHorario/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] dados = Console.ReadLine().Split(' ');
+
+for (int i = 0; i < 3; i++)
+{
+    if (dados[i] == "0")
+        dados[i] = "24";
+}
+
+int saida = Convert.ToInt32(dados[0]);
+int duracao = Convert.ToInt32(dados[1]);
+int fuso = Convert.ToInt32(dados[2]);
+
+
+int resultado = saida + duracao + fuso;
+
+if (resultado > 24)
+    Console.WriteLine(resultado - 24);
+else if (resultado < 24)
+    Console.WriteLine(resultado);
+else
+    Console.WriteLine(0);
