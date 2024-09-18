29cf6779bf600e5c96c22109e3eb99bdc006fe67
Higor Metodio <higor.metodio@outlook.com>
2024-09-18 10:39:05 -0300

    solucao problema 2031-Pedra Papel e Ataque Aereo

---
+++ iniciante/2031_PedraPapelAtaque/Program.cs
@@ -0,0 +1,24 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+
+while (N > 0)
+{
+    string jogador1 = Console.ReadLine();
+    string jogador2 = Console.ReadLine();
+
+    if (jogador1 == "ataque" && jogador2 != "ataque")
+        Console.WriteLine("Jogador 1 venceu");
+    else if (jogador2 == "ataque" && jogador1 != "ataque")
+        Console.WriteLine("Jogador 2 venceu");
+    else if (jogador1 == "pedra" && jogador2 == "papel")
+        Console.WriteLine("Jogador 1 venceu");
+    else if (jogador2 == "pedra" && jogador1 == "papel")
+        Console.WriteLine("Jogador 2 venceu");
+    else if (jogador1 == "papel" && jogador2 == "papel")
+        Console.WriteLine("Ambos venceram");
+    else if (jogador1 == "pedra" && jogador2 == "pedra")
+        Console.WriteLine("Sem ganhador");
+    else
+        Console.WriteLine("Aniquilacao mutua");
+
+    N--;
+}
